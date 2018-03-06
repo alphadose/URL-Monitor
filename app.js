f956a8b26f0fcda56c98892ad74c29d65dbb9915
Alphadose <anish.mukherjee1996@gmail.com>
2018-03-06 16:56:05 +0530

    Updated main server file

--- app.js
+++ app.js
@@ -4,9 +4,10 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var blob = require('./models/blobs');
 
 var index = require('./routes/index');
-var users = require('./routes/users');
+var blobs = require('./routes/blobs');
 
 var app = express();
 
@@ -18,12 +19,12 @@ app.set('view engine', 'jade');
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
-app.use('/users', users);
+app.use('/blobs', blobs);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
