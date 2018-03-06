919ffd5cb862f00bf48495328613ba63a742c157
Alphadose <anish.mukherjee1996@gmail.com>
2018-03-07 00:52:34 +0530

    Increased parameters for function call

--- controllers/UpdateController.js
+++ controllers/UpdateController.js
@@ -5,7 +5,9 @@ async function update(req, res) {
     var url = req.body.url;
     var method = req.body.method;
     var data = req.body.data;
-    msg = await model(id, url, method, data);
+    var headers = req.body.headers;
+    var time = req.body.time;
+    msg = await model(id, url, method, data, headers, time);
     res.send(msg);
 }
 
